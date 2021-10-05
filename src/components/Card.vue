4f5c7d3f7a9fa7b82c6952551c22896281de2749
alessandroNessi <alessandronessi@msn.com>
2021-10-05 16:48:02 +0200

    bootstrap glitch?

---
+++ src/components/Card.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="card">{{this.filmObject.title}}</div>
+</template>
+
+<script>
+export default {
+    name: 'Card',
+    props:{
+        filmObject:Object,
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    // .card{
+        // height: 10rem;
+        // margin: 10px;
+    // }
+</style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,42 +1,54 @@
 <template>
   <main>
-      <div class="container">{{stringPassed}}</div>
+      <div class="container">{{stringPassed}}
+          <div class="row">
+              <Card class="p-2  col-xl-4 col-md-6 col-12" v-for="(element, index) in this.filmArray" :key="index" :filmObject="element" />
+          </div>
+      </div>
   </main>
 </template>
 
 <script>
 import axios from 'axios';
+// import bootstrap from 'bootstrap';
+import Card from './Card.vue';
 export default {
     name: 'Main',
+    components: {
+        Card,
+    },
     props:{
         stringPassed: String,
     },
     data(){
         return{
             callRoot:"https://api.themoviedb.org/3/search/movie",
-
+            filmArray:[],
         }
     },
     methods:{
 
     },
-    updated(){
+    beforeUpdate(){
         axios.get(this.callRoot, {
             params: {
                 api_key: "a21aee6674cb415ea0fe118a1c90c893",
                 language: "it",
                 query: this.stringPassed,
             }   
-        }).then(function (response) {
-            console.log(response.data.results);
+        }).then((response)=>{
+            this.filmArray=response.data.results;
+            console.log(this.filmArray);
         });
     }
 }
 </script>
 
 <style lang="scss" scoped>
+    @import '~bootstrap/scss/bootstrap.scss';
     main{
         height: calc(100vh - 13rem);
         background-color: peachpuff;
+        overflow: auto;
     }
 </style>
