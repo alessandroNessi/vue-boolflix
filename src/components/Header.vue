af1f7dbcec4a8a1ecda64a67b6ed4d9b98467e25
alessandroNessi <alessandronessi@msn.com>
2021-10-05 13:14:48 +0200

    emit and prop passed done

---
+++ src/components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+  <header>
+      <input id="searchInput" type="text">
+      <button @click="callEmit">submit</button>
+  </header>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+    data(){
+        return{
+
+        }
+    },
+    methods:{
+        callEmit(){
+            const emitValue= document.getElementById("searchInput").value;
+            if(emitValue!=""){
+                this.$emit('emittedSearch',emitValue);
+            }
+        }
+    }
+}
+</script>
+
+<style scoped lang="scss">
+    header{
+        height: 13rem;
+        background-color: palegreen;
+    }
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,32 @@
+<template>
+  <main>
+      <div class="container">{{stringPassed}}</div>
+  </main>
+</template>
+
+<script>
+export default {
+    name: 'Main',
+    props:{
+        stringPassed: String,
+    },
+    data(){
+        return{
+
+        }
+    },
+    methods:{
+
+    },
+    computed:{
+
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    main{
+        height: calc(100vh - 13rem);
+        background-color: peachpuff;
+    }
+</style>
