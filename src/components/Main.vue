2fb6cfdffd99eb9cf6f04650651ecf6d97c0a710
alessandroNessi <alessandronessi@msn.com>
2021-10-06 22:02:49 +0200

    aggiunto filtro per genere

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -5,7 +5,7 @@
           <div class="row_title">
             <h2>FILMS</h2>
             <!-- v-model="this.filmsel" -->
-            <select  @change="Filtering(document.getElementById('filmselected').value,this.filmsArray)" name="filmselect" id="filmselect">
+            <select  @change="Filtering('film')" name="filmselect" id="filmselect">
                 <option value="">tutti i generi</option>
                 <option v-for="(element,index) in this.filmsGenres" :key="index" :value="element.id">{{element.name}}</option>
             </select>
@@ -17,13 +17,13 @@
         <!-- series -->
           <div class="row_title">
             <h2>SERIE</h2>
-            <select name="serieselect" id="serieselect">
+            <select @change="Filtering('serie')" name="serieselect" id="serieselect">
                 <option value="">tutti i generi</option>
                 <option v-for="(element,index) in this.seriesGenres" :key="index" :value="element.id">{{element.name}}</option>
             </select>
           </div>
           <div class="row">
-              <Card class="card__serie col-xxl-2 col-xl-3 col-lg-4 col-md-6 col-12" v-for="(element, index) in this.seriesArray" :key="index" :filmObject="element" />
+              <Card class="card__serie col-xxl-2 col-xl-3 col-lg-4 col-md-6 col-12" v-for="(element, index) in this.seriesArray" :key="index" :class="{'d-none':!element.visibility}" :filmObject="element" />
           </div>
       </div>
   </main>
@@ -44,15 +44,24 @@ export default {
     },
     data(){
         return{
-            filmsel:"",
+            // filmsel:"",
         }
     },
     methods:{
-        Filtering(genre, array){
-            let type=parseInt(genre);
-            console.log(this.filmsel);
+        Filtering(type){
+            let genre,array;
+            if(type=='film'){
+                genre=parseInt(document.getElementById("filmselect").value);
+                array=this.filmsArray;
+            }else if(type=='serie'){
+                genre=parseInt(document.getElementById("serieselect").value);
+                array=this.seriesArray;
+            }
+            // console.log("genere"+genre);
+            // console.log("array"+array);
             array.forEach((element) => {
-                if(element.genre_ids.includes(type)){
+                // console.log("element"+element);
+                if(element.genre_ids.includes(genre)){
                     element.visibility=true;
                 }else{
                     element.visibility=false;
